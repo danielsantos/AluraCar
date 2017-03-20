b97e890f48877fd692a39ea52fb969a0d188e1de
daniel.rocha <danielsantosr.rj@gmail.com>
2017-03-20 17:50:00 -0300

    criacao a opcao de escolher acessorios para o carro

--- www/js/controller.js
+++ www/js/controller.js
@@ -11,4 +11,38 @@ angular.module('starter')
 	
 	$scope.carroEscolhido = angular.fromJson($stateParams.carro);
 	
+	$scope.listaDeAcessorios = [{"nome" : "Freio ABS", "preco" : 800},
+							    {"nome" : "Ar Condicionado", "preco" : 1000},
+							    {"nome" : "MP3 Player", "preco" : 500}];							
+								
+	
+	$scope.mudou = function(acessorio, isMarcado) {
+		
+		if (isMarcado) {
+
+			$scope.carroEscolhido.preco = $scope.carroEscolhido.preco + acessorio.preco;
+			
+		} else {
+			
+			$scope.carroEscolhido.preco = $scope.carroEscolhido.preco - acessorio.preco;
+			
+		}
+		
+	};
+	
 });
+
+/*
+'BMW 120i',
+'Onix 1.6',
+'Fiesta 2.0',
+'C3 1.0',
+'Uno Fire',
+'Sentra 2.0',
+'Astra Sedan',
+'Vectra 2.0 Turbo',
+'Hilux 4x4',
+'Montana Cabine Dupla',
+'Outlander 2.4',
+'Fusca 1500'
+*/
